7732b771bff0041c52e1691a572f3e6368c10958
kai.kang <kang@1234>
2021-07-30 09:18:07 +0800

    输入对话框+信号

---
+++ GUI/对话框操作/文本输入对话框/值发生改变信号.py
@@ -0,0 +1,29 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(500, 500)
+        self.setup_ui()
+
+    def setup_ui(self):
+        input = QInputDialog(self,Qt.FramelessWindowHint)
+        input.setOption(QInputDialog.UseListViewForComboBoxItems)
+        input.setLabelText('请输入你的姓名')
+        input.setOkButtonText('好的')
+        input.setCancelButtonText('我想取消')
+
+        input.textValueChanged.connect(lambda val:print(val))
+        input.show()
+
+
+
+
+
+app = QApplication(sys.argv)
+window = Window()
+
+window.show()
+sys.exit(app.exec_())
---
+++ GUI/对话框操作/文本输入对话框/设置输入类型.py
@@ -0,0 +1,32 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(500, 500)
+        self.setup_ui()
+
+    def setup_ui(self):
+        input = QInputDialog(self,Qt.FramelessWindowHint)
+        input.setOption(QInputDialog.UseListViewForComboBoxItems)
+        #input.setInputMode(QInputDialog.DoubleInput)  #表示只能输入浮点型
+        #input.setInputMode(QInputDialog.InputMode)  #表示只能输入整数
+        input.setInputMode(QInputDialog.TextInput) #表示只能输入文本
+        input.setLabelText('请输入你的姓名')
+        input.setOkButtonText('好的')
+        input.setCancelButtonText('我想取消')
+
+        input.textValueChanged.connect(lambda val:print(val))
+        input.show()
+
+
+
+
+
+app = QApplication(sys.argv)
+window = Window()
+
+window.show()
+sys.exit(app.exec_())
